3b045cae3e887e75eed5516b85e7d3d036fc41a4
tanyaKalra-8 <108450417+tanyaKalra-8@users.noreply.github.com>
2023-05-22 17:15:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const sizeElement =  document.createElement("h1");
+sizeElement.innerText = `Width: ${window.innerWidth} 
+and Height: &{window.innerHeight}`;
+
+document.body.append(sizeElement);
+
+window.onresize = function () {
+	sizeElement.innerText = `Width: ${window.innerWidth} 
+	and Height: ${window.innerHeight}`;
+}
